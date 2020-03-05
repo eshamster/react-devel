511087eb8f518ccd6c3e4eb47d2387e00bd37d15
eshamster <hamgoostar@gmail.com>
2020-03-05 23:24:36 +0900

    Add settings for company-mode
    Cf. https://qiita.com/kod314/items/3a31719db27a166d2ec1

--- react-devel.el
+++ react-devel.el
@@ -15,3 +15,14 @@
 (setq company-tooltip-align-annotations t)
 
 (add-hook 'js-mode-hook #'setup-tide-mode)
+
+;; --- company --- ;;
+
+(use-package company
+  :init
+  (setq company-auto-expand t)
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 2)
+  (setq company-selection-wrap-around t)
+  (setq completion-ignore-case t)
+  (define-key company-active-map (kbd "C-S-h") 'company-show-doc-buffer))
